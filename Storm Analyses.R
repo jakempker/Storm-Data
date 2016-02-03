63d3ee0038ace41fd8d632e5aba9d614146279c8
jkempker <jordan.kempker@gmail.com>
2016-02-03 09:46:41 -0500

    Final Commit

--- Storm Analyses.R
+++ Storm Analyses.R
@@ -11,33 +11,54 @@ if (!file.exists("./StormData.csv.bz2")){
 #read.csv() can handle files with bz2 compression and does not need an extra unzip() step
 #I set stringsAsFactors = FALSE since the altenrative changes all the date-times into factors
 storm <- read.csv("./StormData.csv.bz2", stringsAsFactors = FALSE)
+storm <- select(storm, EVTYPE, FATALITIES, INJURIES, PROPDMGEXP, PROPDMG, CROPDMGEXP, CROPDMG)
+
+table(is.na(storm$PROPDMGEXP))
+table(is.na(storm$CROPDMGEXP))
+
+table(storm$PROPDMGEXP)
+table(storm$CROPDMGEXP)
+
+summary(storm$CROPDMG)
+table(is.na(storm$CROPDMGEXP))
+table(storm$CROPDMGEXP)
+
+storm <- storm %>% mutate(prop_damage = ifelse(PROPDMG == 0, 0,
+                                        ifelse(PROPDMGEXP == "K", 1000 * PROPDMG,
+                                        ifelse(PROPDMGEXP == "k", 1000 * PROPDMG,
+                                        ifelse(PROPDMGEXP == "M", 1000000 * PROPDMG,
+                                        ifelse(PROPDMGEXP == "m", 1000000 * PROPDMG, 
+                                        ifelse(PROPDMGEXP == "B", 1000000000 * PROPDMG, NA)))))))
+
+
+storm <- storm %>% mutate(crop_damage = ifelse(CROPDMG ==0, 0,
+                                        ifelse(CROPDMGEXP == "K", 1000 * CROPDMG,
+                                        ifelse(CROPDMGEXP == "k", 1000 * CROPDMG,
+                                        ifelse(CROPDMGEXP == "M", 1000000 * CROPDMG,
+                                        ifelse(CROPDMGEXP == "m", 1000000 * CROPDMG,
+                                        ifelse(CROPDMGEXP == "B", 1000000000 * CROPDMG, NA)))))))
+
+summary(storm$prop_damage)
+summary(storm$crop_damage) 
 
 str(storm)
 head(storm)
-
-#First, to change the date-times into an appropriate format
-#Then extract the year so we can get at some trends.
-storm$BGN_DATE <- as.POSIXlt(strptime(storm$BGN_DATE, format = "%m/%d/%Y %X"))
-#the POSIXlt allows me to extract teh year
-storm <- mutate(storm, year = BGN_DATE$year)
-storm <- mutate(storm, year = year + 1900)
-#dplyr doesn't seem to support POSIXlt anywhere else so I convert back to POSIXct
-storm$BGN_DATE <- as.POSIXct(storm$BGN_DATE)
 
 #Factor Event type
 storm$EVTYPE <- as.factor(storm$EVTYPE)
 #This produces 985 event types!
 levels(storm$EVTYPE)
+storm <- group_by(storm, EVTYPE)
 
 #Let's try some summation of deaths across event types
 
-storm <- group_by(storm, EVTYPE)
 human_impact <- summarise(storm, TotalDeath = sum(FATALITIES), TotalInjury = sum(INJURIES), TotalImpact = sum(TotalDeath, TotalInjury))
 human_impact <- filter(human_impact, TotalImpact > 0)
 human_impact <- arrange(human_impact, desc(TotalImpact, TotalDeath))
-View(head(human_impact, n = nrow(human_impact)/5))
 
-ggplot(head(human_impact, n = nrow(human_impact)/30), aes(x=EVTYPE, y = TotalImpact)) + 
+
+View(head(human_impact, n = nrow(human_impact)/5))
+ggplot(head(human_impact, n = nrow(human_impact)/10), aes(x=EVTYPE, y = TotalImpact)) + 
     geom_bar(stat = "identity") +
     xlab("Event Type") +
     ylab("Total Number of Human Injuries + Deaths")+
@@ -45,20 +66,18 @@ ggplot(head(human_impact, n = nrow(human_impact)/30), aes(x=EVTYPE, y = TotalImp
 
 
 #Now to look at economic damage
-# there is property damage and crop damage
-#each is composed of a numeric value and an associated character value that serves as a multiplier
-class(storm$PROPDMGEXP)
-factor(storm$PROPDMGEXP)
-table(storm$PROPDMGEXP)
-class(storm$PROPDMG)
+econ_impact <- summarise(storm, 
+                         TotalProp = sum(prop_damage), 
+                         TotalCrop = sum(crop_damage), 
+                         TotalDamage = sum(TotalProp, TotalCrop))
 
-table(is.na(storm$PROPDMGEXP))
-table(is.na(storm$CROPDMGEXP))
+econ_impact <- filter(econ_impact, TotalDamage > 0)
+econ_impact <- arrange(econ_impact, desc(TotalDamage))
 
 
-storm <- storm %>% mutate(prop_damage = ifelse(PROPDMGEXP == "K", 1000 * PROPDMG,
-                                ifelse(PROPDMGEXP == "k", 1000 * PROPDMG,
-                            ifelse(PROPDMGEXP == "B", 1000000000 * PROPDMG,
-                            ifelse(PROPDMGEXP == "M", 1000000 * PROPDMG,
-                            ifelse(PROPDMGEXP == "m", 1000000 * PROPDMG, NA))))))
-    
+View(head(econ_impact, n=nrow(econ_impact)/5))
+ggplot(head(econ_impact, n=10), aes(x=EVTYPE, y = TotalDamage)) +
+    geom_bar(stat = 'identity') +
+    xlab("Event Type") +
+    ylab("Estimated Dollar Amount of Damages") +
+    ggtitle("Sum of Estimated Dollar Amount of Crop and Property Damage by Storm Type, US 1950-2011")
